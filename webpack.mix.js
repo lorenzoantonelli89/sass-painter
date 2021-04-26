78fe262c2415fa5e4a56b200535cde5472e662fc
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-04-26 16:39:31 +0200

    update main finito sezione 1 e 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ let mix = require('laravel-mix');
 
 mix
     .js('src/main.js', 'dist')
-    .sass('src/master.scss', '')
+    .sass('src/master.scss', '').options({
+        processCssUrls: false
+    })
     .setPublicPath('dist')
     .copy('src/index.html', 'dist');
